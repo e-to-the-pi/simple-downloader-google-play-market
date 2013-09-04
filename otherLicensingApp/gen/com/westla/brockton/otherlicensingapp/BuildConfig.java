94c7f9105abf65530c08660ccc1430396750025f
e-to-the-pi <nesim.sisa@gmail.com>
2013-09-04 15:25:05 -0700

    initial commit, seems to build in Eclipse after adding libraries

---
+++ otherLicensingApp/gen/com/westla/brockton/otherlicensingapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.westla.brockton.otherlicensingapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
